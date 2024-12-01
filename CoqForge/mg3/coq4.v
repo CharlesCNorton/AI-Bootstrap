bc51227b9e9a59b8b4597e8bac117790d6cf1c90
CharlesCNorton <machineelv@gmail.com>
2024-12-01 13:05:36 -0500

    coq

---
+++ CoqForge/mg3/coq4.v
@@ -0,0 +1,16 @@
+Require Import UniMath.Foundations.All.
+Require Import UniMath.CategoryTheory.Core.Categories.
+
+Local Open Scope cat.
+
+Record Suspension (C : category) : UU := {
+  underlying_cat := C;
+  susp_map : ob C -> ob C;
+  susp_mor : ∏ (a b : ob C), (a --> b) -> (susp_map a --> susp_map b);
+  susp_id : ∏ (a : ob C), susp_mor a a (identity a) = identity (susp_map a);
+  susp_comp : ∏ (a b c : ob C) (f : a --> b) (g : b --> c),
+              susp_mor a c (f · g) = susp_mor a b f · susp_mor b c g;
+  base_point : ob C;
+  zero_section : ∏ (a : ob C), base_point --> a;
+  point_susp : base_point --> susp_map base_point
+}.
